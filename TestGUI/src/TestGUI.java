a862989ce73ccaaddd904e218b2feca97edb17e4
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-03-04 21:17:37 +0200

    Add files via upload

---
+++ TestGUI/src/TestGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TestGUI {
+    public static void main(String[] args) {
+
+        MyJButton button1 = new MyJButton("Button");
+        MyJPanel panel = new MyJPanel("Panel", 100, 100, button1);
+        MyJFrame frame = new MyJFrame("Frame", 200, 200, panel);
+    }
+}
