fc373c4da6bd7c211784f8fefbd8a7df3e817aac
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-03-13 21:56:20 +0200

    Add files via upload

---
+++ HeadFirstJava/Shuffle1/src/PoolPuzzleOne.java
@@ -0,0 +1,25 @@
+public class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 4) {
+            System.out.print("a ");
+            if (x < 1) {
+                System.out.print("noise");
+            }
+            x = x + 1;
+
+            if () {
+
+
+            }
+            if (x == 1) {
+
+            }
+            if () {
+
+            }
+            System.out.println("");
+
+        }
+    }
+}
